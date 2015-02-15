62b3abb70744e6e065dbeafaf741d276bac9573f
Shake0 <housedrop@gmail.com>
2015-02-15 15:52:52 -0500

    Input validation
    Validating the input and implement a default behaviour
    according to the assignment

--- PM1Official/src/java/wscat/service/WSDLRetrieveService.java
+++ PM1Official/src/java/wscat/service/WSDLRetrieveService.java
@@ -13,38 +13,19 @@ public class WSDLRetrieveService {
 	will only get the WSDL listing.
 	Therefore for downloading N WDSL, the limit is N + 1
 	*/
-	public static final int DEFAULT_LIMIT = 1;
+	public static final int    DEFAULT_LIMIT = 1;
+	public static final String DEFAULT_URL   = "http://data.serviceplatform.org/wsdl_grabbing/";
 	
 	public List<String> retrieveWSDLs(String pstrSeedURI) throws MalformedURLException{
-		WSDLRetriever parser;
-		try {
-			// By default the Parser do not fork
-			// By default the Parser do not use history
-			parser = new WSDLRetriever(pstrSeedURI, DEFAULT_LIMIT);
-			new ForkJoinPool().invoke(parser);
-			
-			/*
-			TODO: From the parser object you should be able
-			to access all the informations that you have grabbed
-			after the "invoke".
-			
-			It's up to you to build an index in MARFCAT-IN.
-			getWSDLDescription gives you a list of "struct Documentation".
-			that allowed me to build a one to many relationship (filename -> {description, ...})
-			
-			If you are crazy, you can do recursive search using reflection
-			in order to support Map.class (in parser). I advice you that once in parser.java
-			it's like the limbo in Inception... (Do you think we can prioritize links for a better search? lool)
-			*/
-			
-			return parser.getWSDL();
-		} catch (NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return retrieveWSDLsS(pstrSeedURI, DEFAULT_LIMIT);
 	}
 	
 	public List<String> retrieveWSDLsS(String pstrSeedURI, int piLimit) throws MalformedURLException{
+		// Input validation
+		pstrSeedURI = (pstrSeedURI != null)?pstrSeedURI:DEFAULT_URL;
+		piLimit     = (piLimit > 0)?piLimit:DEFAULT_LIMIT;
+		
+		// Execution
 		WSDLRetriever parser;
 		try {
 			parser = new WSDLRetriever(pstrSeedURI, piLimit +1);
