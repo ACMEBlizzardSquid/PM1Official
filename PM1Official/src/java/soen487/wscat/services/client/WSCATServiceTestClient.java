20526ebb538523cc3e06af3fd57b2d93d9586203
Simon Larocque <si_laroc@sulston.encs.concordia.ca>
2015-02-18 21:57:57 -0500

    added train and submit tests

--- PM1Official/src/java/soen487/wscat/services/client/WSCATServiceTestClient.java
+++ PM1Official/src/java/soen487/wscat/services/client/WSCATServiceTestClient.java
@@ -1,6 +1,7 @@
 package soen487.wscat.services.client;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
@@ -9,13 +10,15 @@ import soen487.pm1.WSDL;
 
 public class WSCATServiceTestClient {
 
-    public static void main(String[] args) throws IOException_Exception, InterruptedException_Exception{
+    public static void main(String[] args) throws IOException_Exception, InterruptedException_Exception, ParserConfigurationException_Exception, SAXException_Exception{
         WSCATServiceService wscatServiceService = new WSCATServiceService();
         WSCATService wscatService = wscatServiceService.getWSCATServicePort();
         
+        String wsdlRepo = "http://data.serviceplatform.org/wsdl_grabbing/service_repository-wsdls/valid_WSDLs/";
+        String wsdlUri = "http://data.serviceplatform.org/wsdl_grabbing/service_repository-wsdls/valid_WSDLs/5-check.wsdl";
         String wsdl = "";
         try {
-            wsdl = WSDL.getWSDL("http://data.serviceplatform.org/wsdl_grabbing/service_repository-wsdls/valid_WSDLs/");
+            wsdl = WSDL.getWSDL(wsdlRepo);
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(WSCATServiceTestClient.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SAXException ex) {
@@ -24,6 +27,9 @@ public class WSCATServiceTestClient {
             Logger.getLogger(WSCATServiceTestClient.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        wscatService.submitWSDLRepo(wsdl);
+        List<String> wsdls = wscatService.submitWSDLRepo(wsdlRepo);
+        wscatService.trainOnURI(wsdlUri);
+        
+        wscatService.submitWSDLToAnalyze(wsdlUri);
     }
 }
